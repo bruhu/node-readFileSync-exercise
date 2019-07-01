b1f80a0671e7affa5c22b3da5af8e4114af0da59
Bru <annabru.abh@gmail.com>
2019-07-01 13:59:03 +0200

    destructured let with content, console.log prints now name and version

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+//load the fs module to our application
 const fs = require("fs");
 
+//use the readFileSync function and pass it the relative file path to the file that we want to read
 let rawdata = fs.readFileSync("./package.json");
-let displayedInfo = JSON.parse(rawdata);
 
-console.log(displayedInfo);
+//json parse to avoid reading raw hex data
+let { name, version } = JSON.parse(rawdata);
+
+console.log(name, version);
